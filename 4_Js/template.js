a9e12d4597459e595f8292c98ebce9013f142d95
hamitmizrak <hamitmizrak@gmail.com>
2022-10-09 12:32:09 +0300

    sayi

--- 4_Js/template.js
+++ 4_Js/template.js
@@ -160,41 +160,41 @@
 // //isNaN 
 // console.log(isNaN(44))
 
-//new:hafızan yer açar
-//null:hafızayı siler
-//zero:değeri yoktur
+// new:hafızan yer açar
+// null:hafızayı siler
+// zero:değeri yoktur
 
-//escape character 
+// escape character 
 // console.log("1.satır\n2.satır")
 // console.log("3.satır\"4.satır")
 // console.log("4.satır\n\t5.satır")
 
-//break: döngüyü orada sonlandırır.
-//return: metodu orada sonlandırır.
-//continue: sadece 1 kereye o şartın sağlandığında çalışmasın sonra devam etsin
-//debugger: debug için point oluşturmaya yarar.
+// break: döngüyü orada sonlandırır.
+// return: metodu orada sonlandırır.
+// continue: sadece 1 kereye o şartın sağlandığında çalışmasın sonra devam etsin
+// debugger: debug için point oluşturmaya yarar.
 
-//Math
-console.log(Math.min(2,5,6,2,9,11,-965));
-console.log(Math.max(2,5,6,2,9,11,-965));
-console.log(Math.abs(-965));
-console.log(Math.sqrt(16));
-console.log(Math.sqrt(Math.abs(-16)));
-console.log(Math.pow(2,5));
+// Math
+// console.log(Math.min(2,5,6,2,9,11,-965));
+// console.log(Math.max(2,5,6,2,9,11,-965));
+// console.log(Math.abs(-965));
+// console.log(Math.sqrt(16));
+// console.log(Math.sqrt(Math.abs(-16)));
+// console.log(Math.pow(2,5));
 
-//yuvarlamak
-console.log(Math.ceil(4.1));
-console.log(Math.floor(4.9));
-console.log(Math.round(4.4));
-console.log(Math.round(4.5));
+// //yuvarlamak
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.round(4.4));
+// console.log(Math.round(4.5));
 
-//sin cos
-console.log(Math.sin(45));
-console.log(Math.cos(45));
+// //sin cos
+// console.log(Math.sin(45));
+// console.log(Math.cos(45));
 
-//Random
-console.log(Math.random()*4+1);
-console.log(Math.round(Math.random()*4+1));
+// //Random
+// console.log(Math.random()*4+1);
+// console.log(Math.round(Math.random()*4+1));
 
 // Math Ödev
 // Kullanıcıda tarafından aldığımız 2 tane sayının
@@ -203,5 +203,52 @@ console.log(Math.round(Math.random()*4+1));
 // s3-) Bu sayılardan en büyüğü karekökü nedir ?
 // s4-) Bu sayılardan en büyüğü karekökünün üste yuvarlıyalım nedir ?
 // s5-) Bu sayılardan en küçüğü mutlak değeri nedir ?
-// s6-) Bu sayılardan en küçüğü alt taban en büyüğü üst taban şeklinde üstünü alalım
+// s6-) Bu sayılardan en küçüğü alt taban en büyüğü üst taban şeklinde üstünü alalım ?
+
+//SAYI
+let sayi1=55;
+sayi1=14.5263;
+console.log(sayi1);
+console.log(typeof sayi1);
+
+//virgüllü sayı
+console.log(sayi1.toFixed(1));
+
+//bilimsel gösterim
+let bilimselGosterim=13E+5;
+console.log(bilimselGosterim);
+
+let bilimselGosterim2=13E-5;
+console.log(bilimselGosterim2);
+
+const sayi12=45000;
+console.log(sayi12.toExponential(3));
+
+//cast
+//const number41=Number(prompt("sayı girininiz"));
+
+//Infinity
+let data=44/0;
+console.log(data);
+
+//NaN
+let data2=44/"asd";
+console.log(data2);
+
+//Binary: 2 (0-1)
+let binary=0b01011;
+console.log(binary);
+
+//Octal 8 (0-1-2-3-4-5-6-7)
+let octal=07161;
+console.log(octal);
+
+//Decimal 10 (0-1-2-3-4-5-6-7-8-9)
+let decimal=10123;
+console.log(decimal);
+
+//Hexadecimal 16 (0-1-2-3-4-5-6-7-8-9-A-B-C-D-E-F)
+let hexadecimal=0xAbc98;
+console.log(hexadecimal);
+
 
