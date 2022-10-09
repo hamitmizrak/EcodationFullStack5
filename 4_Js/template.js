e4eb993061a3fdf3ca4776708de31feeeab63689
hamitmizrak <hamitmizrak@gmail.com>
2022-10-09 10:18:18 +0300

    javascript

---
+++ 4_Js/template.js
@@ -0,0 +1,36 @@
+//document 
+//window
+
+/*çoklu yorumlar*/
+// camelCase
+// PascalCase
+// underscore 
+// hyphens -
+//""
+//''
+//``
+
+//Çıktı: 
+// console.log("log")
+// console.error("error")
+// console.warn("warn")
+// console.info("info")
+
+//değişkenler
+// var x1="merhabalar";
+// var x1="denmee"
+
+// let x2="merhabalar";
+// const PI=3.141519;
+
+
+var degisken=44;
+var degisken41=44;
+var _degisken41=44;
+var $degisken41=44;
+var degiskenAdi=44;
+
+// var +degisken41=44;
+// var 545degisken41=44;
+
+
