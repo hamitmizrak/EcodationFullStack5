bd697ebd94d7bb19123dcdfd3f915aac081ff153
hamitmizrak <hamitmizrak@gmail.com>
2022-10-09 11:43:24 +0300

    js

--- 4_Js/template.js
+++ 4_Js/template.js
@@ -63,60 +63,60 @@
 
 //operator
 // +
-console.log(4+2);
+// console.log(4+2);
 
-// -
-console.log(6-2);
+// // -
+// console.log(6-2);
 
-// *
-console.log(6*2);
+// // *
+// console.log(6*2);
 
-// **
-console.log(2**5);
-console.log(Math.pow(2,5));
+// // **
+// console.log(2**5);
+// console.log(Math.pow(2,5));
 
-// /
-console.log(9/2);
+// // /
+// console.log(9/2);
 
-// %
-console.log(9%2);
+// // %
+// console.log(9%2);
 
-console.log("***************")
+// console.log("***************")
 
-let counter=0;
-console.log(counter)
-console.log(counter++)
-console.log(counter)
-// x++
+// let counter=0;
+// console.log(counter)
+// console.log(counter++)
+// console.log(counter)
+// // x++
 
-console.log("***************")
+// console.log("***************")
 
-let counter2=0;
-console.log(counter2)
-console.log(++counter2)
-console.log(counter2)
-// ++x
+// let counter2=0;
+// console.log(counter2)
+// console.log(++counter2)
+// console.log(counter2)
+// // ++x
 
-console.log("***************")
+// console.log("***************")
 
-let counter3=5;
-console.log(counter3)
-console.log(counter3--)
-console.log(counter3)
-// x--
+// let counter3=5;
+// console.log(counter3)
+// console.log(counter3--)
+// console.log(counter3)
+// // x--
 
-console.log("***************")
+// console.log("***************")
 
-let counter4=5;
-console.log(counter4)
-console.log(--counter4)
-console.log(counter4)
+// let counter4=5;
+// console.log(counter4)
+// console.log(--counter4)
+// console.log(counter4)
 // y--
 // --y
 
 //!
-let data=true; 
-console.log(!data)
+// let data=true; 
+// console.log(!data)
 // ||=VEYA  &=VE
 
 // =   -> ATAMA
@@ -127,4 +127,39 @@ console.log(!data)
 // 4==="4" sonuc eşit değildir
 
 
+//Kullanıcıdan veri almak
+// let user=prompt("Lütfen isminiz giriniz");
+// console.log(user);
+// console.log( typeof userData);
 
+
+// //string -> number (Number)
+// //number -> string (String)
+
+// let userData=prompt("Lütfen sayı giriniz");
+// console.log(Number(userData**userData));
+// console.log( typeof userData);
+
+// let userData2=prompt("Lütfen sayı giriniz");
+// console.log(String(userData2**userData2));
+// console.log( typeof userData);
+
+//undefined
+var variable;
+console.log(variable);
+
+//Infinity
+var variable2=4/0;
+console.log(variable2);
+
+//NaN: not A number
+var variable3=4/"asd";
+console.log(variable3);
+
+
+//isNaN 
+console.log(isNaN(44))
+
+//new:hafızan yer açar
+//null:hafızayı siler
+//zero:değeri yoktur
